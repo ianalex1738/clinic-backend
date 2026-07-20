86908ccace5e2ac4c53fb07c85d9f3232dcbb4b5
Ian Alex <105561058+ianalex1738@users.noreply.github.com>
2026-07-21 02:05:03 +0300

    Update CORS configuration to allow multiple origins

--- src/server.js
+++ src/server.js
@@ -9,7 +9,13 @@ import apptRoutes   from './routes/appointments.js';
 const app  = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    process.env.CLIENT_URL,
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth',         authRoutes);
